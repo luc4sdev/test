ac1ac19133159e631a8168925cf83082367d9cb7
lucas <lucas.pfranca01@gmail.com>
2024-05-24 22:13:57 -0300

    feat: add changes

--- src/use-cases/create-use-case.ts
+++ src/use-cases/create-use-case.ts
@@ -2,6 +2,8 @@
 //test
 export interface CreateClientUseCaseRequest {
     name: string
+    game: string
+    title: string
     type: "FISICA" | "JURIDICA"
     document: string
     birthDate: string
@@ -19,12 +21,14 @@ export class CreateClientUseCase {
 
     constructor(private clientsRepository: any) { }
 
-    async execute({ name, type, document, birthDate, address }: CreateClientUseCaseRequest): Promise<any> {
+    async execute({ name, type, document, birthDate, address, game, title }: CreateClientUseCaseRequest): Promise<any> {
 
-        const client = await this.clientsRepository.create({ name, type, document, birthDate, address })
+        const client = await this.clientsRepository.create({ name, type, document, birthDate, address, game, title })
 
+        const abc = title.toString().toLocaleUpperCase()
         return {
-            client
+            client,
+            abc
         }
 
     }
